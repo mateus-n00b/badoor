7fb442cceecf9c36060583db06fbe59d20b75c78
mateus <mateus.aluufc@gmail.com>
2017-02-11 21:25:27 -0300

    Update badoor_server.c

--- badoor_server.c
+++ badoor_server.c
@@ -1,6 +1,7 @@
 #include "stdio.h"
 #include "string.h"
 
-main(int argc, char const *argv[]){
-    system("python3 -m http.server 8000 &");
+main(int argc, char const *argv[]){    
+    system("python3 -m http.server 8000");
+    system("zenity --error --text 'File badass.c not found!'");
 }
